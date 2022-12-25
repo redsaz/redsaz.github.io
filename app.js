c8c7a3664233d7d0fea5ba71a0fde13640541092
redsaz <redsaz@gmail.com>
2022-12-25 00:35:01 -0600

    Update chain-drop

--- app.js
+++ app.js
@@ -130,20 +130,59 @@ class SceneMultitouch extends Phaser.Scene {
         });
     }
 }
-class SceneLevelClear extends Phaser.Scene {
+// Displays the menu at the conclusion of a level
+// (like, a button to the next level if won, or a button to retry or go to game menu)
+class SceneLevelDoneMenu extends Phaser.Scene {
     constructor(config) {
         super(config);
+        this.controlsEvents = new Phaser.Events.EventEmitter();
     }
     preload() {
-    }
-    create(data) {
         var _a, _b;
-        var text = this.add.text(400, 300, 'CLEAR!', { fontSize: '66px', fontFamily: 'Sans-Serif', fontStyle: 'bold', color: '#fff', stroke: '#000', strokeThickness: 10, align: 'center' });
-        text.setX(this.cameras.default.centerX - (text.width / 2));
-        text.setY(this.cameras.default.centerY - (text.height / 2));
+        let stroke = 4;
+        let half_stroke = stroke / 2;
+        let xPos = 281;
+        let width = 236;
+        let yPos = 422;
+        let height = 150;
+        let rounding = 16;
+        let graphics = this.add.graphics();
+        let text = this.add.text(xPos, yPos + (height / 2) - 26, 'NEXT', { fixedWidth: width, fontSize: '32px', fontFamily: 'Sans-Serif', fontStyle: 'bold', color: '#fff', stroke: '#000', strokeThickness: 10, align: 'center' });
+        let box = this.add.zone(xPos + (width / 2), yPos + (height / 2), width, height);
+        this.drawButton(graphics, text, xPos, yPos, width, height);
+        this.input.on('pointermove', (pointer) => {
+            // If the press event was outside the button, then how button appears will not change
+            // even if the pointer moves over the button. We can leave early.
+            if (!box.getBounds().contains(pointer.downX, pointer.downY)) {
+                return;
+            }
+            let oldPos = pointer.prevPosition;
+            // If pressed, and position *was* outside the button but is now in the button, then show the button pressed.
+            if (pointer.primaryDown && box.getBounds().contains(pointer.x, pointer.y) && !box.getBounds().contains(oldPos.x, oldPos.y)) {
+                this.drawButtonPressing(graphics, text, xPos, yPos, width, height);
+            }
+            else if (pointer.primaryDown && !box.getBounds().contains(pointer.x, pointer.y) && box.getBounds().contains(oldPos.x, oldPos.y)) {
+                // else, if pressed and position *was* in the button but is now outside the button, then show the button unpressed.
+                this.drawButton(graphics, text, xPos, yPos, width, height);
+            }
+        });
+        this.input.on('pointerdown', (pointer) => {
+            if (box.getBounds().contains(pointer.downX, pointer.downY)) {
+                this.drawButtonPressing(graphics, text, xPos, yPos, width, height);
+            }
+        });
+        this.input.on('pointerup', (pointer) => {
+            // If the pointer was pressed on the button, and released on the button,
+            // then activate the button.
+            if (box.getBounds().contains(pointer.x, pointer.y) && box.getBounds().contains(pointer.downX, pointer.downY)) {
+                this.goNext();
+            }
+            this.drawButton(graphics, text, xPos, yPos, width, height);
+        });
         (_a = this.input.keyboard) === null || _a === void 0 ? void 0 : _a.addKey(13, false, false).on('down', this.goNext, this);
         (_b = this.input.keyboard) === null || _b === void 0 ? void 0 : _b.addKey(32, false, false).on('down', this.goNext, this);
-        gameThingies = data;
+    }
+    create(data) {
     }
     update(time, delta) {
     }
@@ -153,7 +192,45 @@ class SceneLevelClear extends Phaser.Scene {
             gameThingies.gameSettings.level++;
         }
         this.scene.get('SceneGrid').scene.restart();
+        this.scene.get('SceneLevelClear').scene.stop();
         this.scene.stop(this.scene.key);
+    }
+    drawButton(graphics, text, xPos, yPos, width, height) {
+        let stroke = 4;
+        let half_stroke = stroke / 2;
+        let rounding = 16;
+        graphics.clear();
+        graphics.fillStyle(0x555555, 1).fillRoundedRect(xPos + half_stroke, yPos + half_stroke, width - stroke, height - stroke, rounding);
+        graphics.fillGradientStyle(0x555555, 0x555555, 0x999999, 0x999999).fillRect(xPos + half_stroke, yPos + half_stroke + 10, width, (height - 20 - stroke) / 2);
+        graphics.fillGradientStyle(0x999999, 0x999999, 0x555555, 0x555555).fillRect(xPos + half_stroke, yPos + half_stroke + ((height - 10) / 2), width - stroke, (height - 20 - stroke) / 2);
+        graphics.lineStyle(stroke, 0xaaaaaa, 1).strokeRoundedRect(xPos + half_stroke, yPos + half_stroke, width - stroke, height - stroke, rounding);
+        text.setY(yPos + (height / 2) - 26);
+    }
+    drawButtonPressing(graphics, text, xPos, yPos, width, height) {
+        let stroke = 4;
+        let half_stroke = stroke / 2;
+        let rounding = 16;
+        graphics.clear();
+        graphics.fillStyle(0x999999, 1).fillRoundedRect(xPos + half_stroke, yPos + half_stroke, width - stroke, height - stroke, rounding);
+        graphics.fillGradientStyle(0x999999, 0x999999, 0x555555, 0x555555).fillRect(xPos + half_stroke, yPos + half_stroke + 10, width, (height - 20 - stroke) / 2);
+        graphics.fillGradientStyle(0x555555, 0x555555, 0x999999, 0x999999).fillRect(xPos + half_stroke, yPos + half_stroke + ((height - 10) / 2), width - stroke, (height - 20 - stroke) / 2);
+        graphics.lineStyle(stroke, 0xeeeeee, 1).strokeRoundedRect(xPos + half_stroke, yPos + half_stroke, width - stroke, height - stroke, rounding);
+        text.setY(yPos + (height / 2) - 25);
+    }
+}
+class SceneLevelClear extends Phaser.Scene {
+    constructor(config) {
+        super(config);
+    }
+    preload() {
+        var text = this.add.text(400, 300, 'CLEAR!', { fontSize: '66px', fontFamily: 'Sans-Serif', fontStyle: 'bold', color: '#fff', stroke: '#000', strokeThickness: 10, align: 'center' });
+        text.setX(this.cameras.default.centerX - (text.width / 2));
+        text.setY(this.cameras.default.centerY - (text.height / 2));
+    }
+    create(data) {
+        gameThingies = data;
+    }
+    update(time, delta) {
     }
 }
 class SceneLevelLost extends Phaser.Scene {
@@ -1051,6 +1128,9 @@ class SceneGrid extends Phaser.Scene {
                     if (!this.scene.isActive("SceneLevelClear")) {
                         this.scene.run("SceneLevelClear");
                     }
+                    if (!this.scene.isActive("SceneLevelDoneMenu")) {
+                        this.scene.run("SceneLevelDoneMenu");
+                    }
                     break;
                 }
             }
@@ -1144,3 +1224,4 @@ GAME.scene.add('SceneGrid', SceneGrid, true, gameThingies);
 GAME.scene.add('SceneMultitouch', SceneMultitouch, true, gameThingies);
 GAME.scene.add('SceneLevelClear', SceneLevelClear, false, gameThingies);
 GAME.scene.add('SceneLevelLost', SceneLevelLost, false);
+GAME.scene.add('SceneLevelDoneMenu', SceneLevelDoneMenu, false, gameThingies);
